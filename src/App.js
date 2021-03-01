97697f4ba76f15d047ee8d74030ea22c07dffe10
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-03-01 16:33:14 +0530

    create app footer and its styles

--- src/App.js
+++ src/App.js
@@ -11,6 +11,53 @@ class App extends React.Component{
                     COVID 19 Live TRACKER <span className={'header-light'}>by kavinduakash.com</span>
                 </div>
             </div>
+            <div className={'app__content'}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet, architecto asperiores autem consequuntur cumque doloribus eaque enim fugiat itaque laborum maxime neque porro quidem rerum similique, tempore temporibus totam!
+            </div>
+            <div className={'app__footer'}>
+                <div className={'footer'}>
+                    COVID 19 Live TRACKER <span className={'footer-light'}>by kavinduakash.com</span>
+                </div>
+            </div>
         </div>
     )
   }
