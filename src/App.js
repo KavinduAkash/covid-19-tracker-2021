d79163f62b3d6d93798f530d695b3e11dd320914
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-03-01 16:01:13 +0530

    make codes for top header and apply styles

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ class App extends React.Component{
   render() {
     return (
         <div className="app">
+            <div className={'app__top'}>
+                <div className={'header'}>
+                    COVID 19 Live TRACKER <span className={'header-light'}>by kavinduakash.com</span>
+                </div>
+            </div>
         </div>
     )
   }
