7429b4f9fdee043f1d3166ef0c912f7ad6518217
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-03-05 20:30:36 +0530

    change line chart border and background color

--- src/components/charts/Linerchart.component.js
+++ src/components/charts/Linerchart.component.js
@@ -39,8 +39,8 @@ class Linerchart extends React.Component {
                             // label: "First dataset",
                             data: data_set,
                             fill: true,
-                            backgroundColor: "rgba(75,192,192,0.2)",
-                            borderColor: "rgba(75,192,192,1)"
+                            backgroundColor: "rgba(229, 57, 53, 0.09)",
+                            borderColor: "#e53935"
                         }
                     ]
                 };
